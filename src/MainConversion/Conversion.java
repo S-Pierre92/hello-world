57f91c4dfbe41f99c075a91aa4d7c217fe37d975
Simon Pierre Nkizamacumu <s-pierre92@hotmail.com>
2019-01-05 18:53:46 +0100

    mon premier commit ici

---
+++ src/MainConversion/Conversion.java
@@ -0,0 +1,108 @@
+package MainConversion;
+
+import java.util.Scanner;
+
+public class Conversion {
+	
+	private int option;
+	private int[] toConvert ;
+	private static void out(String chaine)
+	{
+		System.out.print(chaine + "\n");
+	}
+
+	private static String transform(int decimal,int base)
+	{
+		
+		int reste = 0;
+		String rep = " ";
+	
+
+		while(decimal > base - 1)
+		{
+			reste = decimal % base;
+			decimal = decimal / base;
+			
+			if(reste >= 10)
+				rep = rep + toHex(reste);
+			
+			else				
+				rep = rep + reste;
+		}
+
+		if(decimal != 0)
+		{
+			if(decimal >= 10)
+				rep = rep + toHex(decimal);
+			else
+				rep = rep + decimal;
+		}
+
+			
+
+		/**************** inversion de la valeur ****************/
+
+		int longueur = rep.length();
+		int i;
+		String inv = "";
+		
+		for(i = 0; i< longueur; i++)
+		{
+			inv = inv + rep.charAt(longueur - 1 - i);
+		}
+		
+		return inv;
+	}
+
+	private static char toHex(int decimal)
+	{
+		switch(decimal)
+		{
+		case 10: return 'A';
+		case 11: return 'B';
+		case 12: return 'C';
+		case 13: return 'D';
+		case 14: return 'E';
+		case 15: return 'F';
+		default: return 'X';
+		}
+	}
+
+	public String[] Result()
+	{
+		int size = toConvert.length,i;
+		String [] toReturn = new String [size];
+		for(i = 0; i < size; i++)
+		{
+			if(option == 1)
+				toReturn[i] = transform(toConvert[i],2); 
+			else
+				toReturn[i] = transform(toConvert[i],16);
+		}
+		
+		return toReturn;
+	}
+	
+	public Conversion(int[] tab)
+	{
+		toConvert = tab;
+	}
+	
+	public void displayWelcome()
+	{
+		Scanner sc = new Scanner(System.in);
+		out("Programme de conversion de base");
+		out("\n1: Décimal -> binaire");
+		out("\n2: Décimal -> hexadécimal");
+		option = sc.nextInt();
+		
+		while(option != 1 && option != 2)
+		{
+			out("Option inexistante! Veuillez en choisir une correcte : ");
+			option = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+	
+}
---
+++ src/MainConversion/MainConversion.java
@@ -0,0 +1,43 @@
+package MainConversion;
+
+import java.util.Scanner;
+
+public class MainConversion {
+
+	private static void out(String chaine)
+	{
+		System.out.print(chaine + "\n");
+	}
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		out("Combien de nombre à traduire?");
+		int size = sc.nextInt(),i;
+		int [] number = new int[size];
+		
+		for(i = 0; i< size; i++)
+		{
+			int pos = i+1;
+			out("Nombre " + pos +":");
+			number[i] = sc.nextInt();
+		}
+		
+		out("...Encodage terminé!");
+		
+		Conversion C = new Conversion(number);
+		
+		C.displayWelcome();
+		String []resultString = C.Result();
+		
+		for(i = 0; i< size; i++)
+		{
+			int pos = i+1;
+			out("resultat "+ pos + ": " + number[i] + " =>  "+resultString[i]);
+		}
+		
+		sc.close();
+		
+	}
+}
